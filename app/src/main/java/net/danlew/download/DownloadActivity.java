01a3e728e8e7f2abf3f13f96d2c2e0ee2cfae1a4
Dan Lew <danlew42@gmail.com>
2014-08-10 22:09:53 -0500

    Added bulk of app

---
+++ app/src/main/java/net/danlew/download/DownloadActivity.java
@@ -0,0 +1,49 @@
+package net.danlew.download;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.webkit.URLUtil;
+import android.widget.Toast;
+
+public class DownloadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        Uri uri = null;
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_SEND)) {
+            CharSequence text = intent.getCharSequenceExtra(Intent.EXTRA_TEXT);
+            CharSequence subject = intent.getCharSequenceExtra(Intent.EXTRA_SUBJECT);
+
+            if (!TextUtils.isEmpty(text) && URLUtil.isNetworkUrl(text.toString())) {
+                uri = Uri.parse(text.toString());
+            }
+            else if (!TextUtils.isEmpty(subject) && URLUtil.isNetworkUrl(subject.toString())) {
+                uri = Uri.parse(subject.toString());
+            }
+        }
+        else if (action.equals(Intent.ACTION_VIEW)) {
+            uri = intent.getData();
+        }
+
+        if (uri != null) {
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            dm.enqueue(request);
+        }
+        else {
+            Toast.makeText(this, R.string.error_invalid, Toast.LENGTH_LONG).show();
+        }
+
+        finish();
+    }
+
+}
